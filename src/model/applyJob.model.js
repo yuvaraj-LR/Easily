9f8441149efbb53d1750d41923c717601396c5f5
yuvaraj-LR <yuvarajrama121@gmail.com>
2024-01-25 23:40:36 +0530

    adding features

--- src/model/applyJob.model.js
+++ src/model/applyJob.model.js
@@ -1,3 +1,5 @@
+import nodemailer from "nodemailer";
+
 export default class ApplyJob {
 
     constructor(index, name, email, phoneNo, resume) {
@@ -14,9 +16,36 @@ export default class ApplyJob {
         let applicant = new ApplyJob(index, name, email, phoneNo, resume)
 
         jobSeekersDetails.push(applicant);
+        sendMail(name, email);
 
         console.log(jobSeekersDetails, "Job Seeker Array");
     }
+}
+
+async function sendMail(name, email) {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "easilydev17@gmail.com",
+            pass: "thow vtoj chzy xwwy"
+        }
+    });
+
+    const mailOptions = {
+        from: "yuvarajrama121@gmail.com",
+        to: email,
+        subject: "Application Recieved",
+        text: "Thank you, " + name + ". We have received your application. We will get back to you as soon as possible."
+    };
+
+    try {
+        const result = await transporter.sendMail(mailOptions);
+        console.log("Email sent successfullly.");
+
+    } catch (error) {
+        console.log("Email was not sent.", error);
+    }
+    return;
 }
 
 var jobSeekersDetails = [];
